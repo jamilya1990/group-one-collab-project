4cb40e41fc47dbd4ec081dd9a0b4a749baf16538
Edgar <cealex510@gmail.com>
2021-07-07 16:51:54 -0700

    additional code per aleks requests001

--- src/test/java/edgar_package/TC001.java
+++ src/test/java/edgar_package/TC001.java
@@ -4,5 +4,6 @@ public class TC001 {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("more code per Aleks requests");
     }
 }
