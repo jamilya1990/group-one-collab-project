90389fe8a7565236d8990a1436a7c3ed2be57675
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-07-07 17:19:17 -0700

    Initial commit

---
+++ src/test/java/AhmadSabawoonFile/HelloChampions.java
@@ -0,0 +1,8 @@
+package AhmadSabawoonFile;
+
+public class HelloChampions {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Everyone, I hope you guys doing well and ok and lets have some githubs");
+    }
+}
