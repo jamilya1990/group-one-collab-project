7450d66c34ca309d3dfc4090d4fd0f1888b6f506
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-07-07 18:34:01 -0700

    Asking help

--- src/test/java/AhmadSabawoonFile/HelloChampions.java
+++ src/test/java/AhmadSabawoonFile/HelloChampions.java
@@ -3,6 +3,6 @@ package AhmadSabawoonFile;
 public class HelloChampions {
 
     public static void main(String[] args) {
-        System.out.println("Hi Everyone, I hope you guys doing well and ok and lets have some githubs");
+        System.out.println("Hi Everyone, I hope you guys doing well and ok and lets have some github");
     }
 }
