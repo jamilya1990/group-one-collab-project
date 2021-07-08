f8f5d9b239cf1565739564998b22bee60d116ebc
AleksandarBacko <backo.backovic94@gmail.com>
2021-07-07 17:37:29 -0700

    aleks more work

--- src/test/java/aleksWork/HelloGroup1.java
+++ src/test/java/aleksWork/HelloGroup1.java
@@ -3,7 +3,7 @@ package aleksWork;
 public class HelloGroup1 {
     public static void main(String[] args) {
         System.out.println("hello everyone ");
-        System.out.println("more work");
+        System.out.println("more work     ");
     }
 
 }
