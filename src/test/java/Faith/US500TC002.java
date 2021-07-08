cfc9717b72b975a6bdb413dc80b5205f102385d6
FaithSlade <faithwslade@gmail.com>
2021-07-07 17:16:16 -0700

    added new class US 500

---
+++ src/test/java/Faith/US500TC002.java
@@ -0,0 +1,8 @@
+package Faith;
+
+public class US500TC002 {
+    public static void main(String[] args) {
+        System.out.println("work has started");
+        System.out.println("squash");
+    }
+}
