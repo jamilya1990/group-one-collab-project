f8ebe812d594e64a2647cd06451b708ed766c3b9
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-07-07 18:34:27 -0700

    Asking help for making the projects

--- src/test/java/AhmadSabawoonFile/HelloChampions.java
+++ src/test/java/AhmadSabawoonFile/HelloChampions.java
@@ -4,5 +4,6 @@ public class HelloChampions {
 
     public static void main(String[] args) {
         System.out.println("Hi Everyone, I hope you guys doing well and ok and lets have some github");
+        //
     }
 }
