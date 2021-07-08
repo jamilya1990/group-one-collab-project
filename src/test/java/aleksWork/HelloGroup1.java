03f78cbc36c8d5aceec12da88932568c000c024c
Edgar <cealex510@gmail.com>
2021-07-07 18:14:06 -0700

    creating conflict with aleks

--- src/test/java/aleksWork/HelloGroup1.java
+++ src/test/java/aleksWork/HelloGroup1.java
@@ -4,6 +4,8 @@ public class HelloGroup1 {
     public static void main(String[] args) {
         System.out.println("hello everyone ");
         System.out.println("more work     ");
+        System.out.println("creating conflict");
+        System.out.println("more conflict");
     }
 
 }
