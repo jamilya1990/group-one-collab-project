875ecee448def538e0d9e3b3ce9bfe7f489dde4b
Edgar <cealex510@gmail.com>
2021-07-07 17:15:20 -0700

    squashed all 4 commits for tc002

---
+++ src/test/java/edgar_package/TC002.java
@@ -0,0 +1,9 @@
+package edgar_package;
+
+public class TC002 {
+    public static void main(String[] args) {
+        System.out.println("started working on TC002");
+        System.out.println("created second branch for TC002");
+        System.out.println("making progress");
+    }
+}
